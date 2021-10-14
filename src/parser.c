8257c1a0ce14872286da62c73dcaf67ceebcb262
gerardalcaina <galcaina98@gmail.com>
2021-10-14 16:45:52 +0200

    Add start parser

---
+++ src/parser.c
@@ -0,0 +1,20 @@
+#include "parser.h"
+
+void read_graph_from_file(FILE *f) {
+    char *line;
+    size_t line_length;
+
+    getline(&line, &line_length, f); // Ignore first three rows of the file
+    getline(&line, &line_length, f);
+    getline(&line, &line_length, f);
+
+    size_t n_nodes = 0;
+    while(getline(&line, &line_length, f) > 0) {
+        if(line[0] == 'n') {
+            n_nodes++;
+        } else {
+            break;
+        }
+    }
+    printf("n_nodes=%zu\n", n_nodes);
+}
---
+++ src/parser.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_graph_from_file(FILE *f);

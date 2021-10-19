87db2d6f85edfc60188a80f35ee123b3fabfb601
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-10-19 21:13:26 +0200

    Add function to search node by `id`

--- src/nodes.c
+++ src/nodes.c
@@ -3,3 +3,13 @@
 void printNode(const Node *node) {
     printf("node: %zu, lat: %lf lon: %lf n_successors: %zu\n", node->id, node->lat, node->lon, node->n_successors);
 }
+
+size_t search_node(const size_t id, const Node *nodes, const size_t n_nodes) {
+    for(size_t iter = 0; iter < n_nodes; iter++) {
+        if(nodes[iter].id == id) {
+            return iter;
+        }
+    }
+
+    return -1;
+}

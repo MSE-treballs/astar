afe656337fb2177f02ff220003f9055ea0aa30c1
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-10-19 20:26:07 +0200

    Add basic functionality test
    Also removes "Hello world!" print

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "parser.h"
 
-int main() {
-   printf("Hello world!\n");
+int main(int argc, char *argv[]) {
+   FILE *f = fopen(argv[1], "r");
+   if(f == NULL) {
+       fprintf(stderr, "Could not find file \"%s\"\n", argv[1]);
+   }
+
+   read_graph_from_file(f);
+
    return 0;
 }

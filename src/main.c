f65af319fb30c0174ed37f962404ff6513c69cf0
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-10-21 19:36:04 +0200

    Fix indentation

--- src/main.c
+++ src/main.c
@@ -2,13 +2,13 @@
 #include "parser.h"
 
 int main(int argc, char *argv[]) {
-   FILE *f = fopen(argv[1], "r");
-   if(f == NULL) {
-       fprintf(stderr, "Could not find file \"%s\"\n", argv[1]);
-   }
+    FILE *f = fopen(argv[1], "r");
+    if(f == NULL) {
+        fprintf(stderr, "Could not find file \"%s\"\n", argv[1]);
+    }
 
-   Node *nodes = NULL;
-   size_t n_nodes = read_graph_from_file(f, &nodes);
+    Node *nodes = NULL;
+    size_t n_nodes = read_graph_from_file(f, &nodes);
 
-   return 0;
+    return 0;
 }

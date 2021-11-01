fc0f4d1439d471f46370d808d975d1ba5da7e4ff
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-11-01 15:13:12 +0100

    Close file after parsing nodes and ways

--- src/main.c
+++ src/main.c
@@ -16,6 +16,8 @@ int main(int argc, char *argv[]) {
     Node *nodes = NULL;
     read_graph_from_file(f, &nodes);
 
+    fclose(f);
+
     Node *from = nodes + 729630;
     Node *to = nodes + 318042;
 

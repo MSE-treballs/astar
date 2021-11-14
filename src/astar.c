2bfe7cae91288a53831f0de32e983bf0db2949a9
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-11-15 00:11:00 +0100

    Add shortcut strategy to `astar` function

--- src/astar.c
+++ src/astar.c
@@ -18,18 +18,36 @@ Bool astar(Node *start, Node *goal) {
 
         for(short foo = 0; foo < current->n_successors; foo++) {
             Node *successor = current->successors[foo];
+
             const double distance = current->distance + get_distance(current, successor);
             if(successor->distance > distance) {
-                const double heuristic = get_heuristic(successor, goal);
-                const double score = distance + heuristic;
-
                 successor->distance = distance;
                 successor->parent = current;
 
+                const double heuristic = get_heuristic(successor, goal);
+                const double score = distance + heuristic;
                 if(successor->open == TRUE) {
                     replace(&queue, successor, score);
                 } else {
                     push(&queue, successor, score);
+                }
+            }
+        }
+
+        for(short foo = 0; foo < current->n_shortcuts; foo++) {
+            Shortcut shortcut = current->shortcuts[foo];
+
+            const double distance = current->distance + shortcut.cost;
+            Node *end = shortcut.end;
+            if(end->distance > distance) {
+                end->distance = distance;
+
+                const double heuristic = get_heuristic(end, goal);
+                const double score = distance + heuristic;
+                if(end->open == TRUE) {
+                    replace(&queue, end, score);
+                } else {
+                    push(&queue, end, score);
                 }
             }
         }
